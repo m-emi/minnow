1b446cc1c70d0f2c87c249cd1c1baf17787009eb
CS144 user <emilio18@stanford.edu>
2024-01-28 07:50:09 +0000

    Fixed initialization issues

--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -25,11 +25,8 @@ protected:
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
   uint64_t capacity_;
   bool error_ {};
-
   std::string buf_ {};
-
   bool closed_ {};
-  
   uint64_t bytes_pushed_ {};
   uint64_t bytes_popped_ {};
 };
