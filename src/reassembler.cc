34bde56c26cda1a77f2e33f1309a6d0351dfe16d
CS144 user <emilio18@stanford.edu>
2024-01-30 01:03:52 +0000

    Implemented the red region

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -9,9 +9,22 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
   (void)data;
   (void)is_last_substring;
 
-  uint64_t first_unassembled_index_ = output_.writer().bytes_pushed();
+
+  // Red region after first unassembled index and before first unacceptable index
+  unassembled_bytes_.resize(output_.writer().available_capacity());
 
 
+
+
+  const uint64_t first_unassembled_index = output_.writer().bytes_pushed();
+
+  // Discard bytes after this index
+  const uint64_t first_unacceptable_index = first_unassembled_index + 
+                  output_.writer().available_capacity();
+
+
+
+// Push assembled bytes
 
 
 }
