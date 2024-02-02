8678e095588abec4decd2de712e3a1c4378e4b81
CS144 user <emilio18@stanford.edu>
2024-02-02 23:47:29 +0000

    Added more comments to tcp_receiver.cc

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -4,20 +4,35 @@ using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
+
+  // To access reassembler, use reassembler_.
+  reassembler_.insert(first_index, message.payload, message.SYN);
+  // Unwrap gets you absolute sequence number
+  uint64_t abseqno = message.seqno.Unwrap(n, zero_point);
+
+
+  
+  if (message.RST) {
+    // use set_error() from bytestream
+  }
+
+
+
   if (message.SYN) {
     // this segment is beginning of the byte stream
     // conversation is starting
     // guaranteed to be sent first
     // don't start conversation until SYN flag is receieved 
+    // first_unassembled_index is checkpoint, AKA bytes_pushed
+    Wrap32 first_seg = message.seqno;
+    uint64_t ISN = first_seg.unwrap(first_seg, writer().bytes_pushed())
+
+
   }
   if (message.FIN) {
     //this segment is the end of the byte stream
     // close conversation once FIN flag is received
   }
-
-insert(message.seqno, message.payload, message.FIN)
-
-
 }
 
 TCPReceiverMessage TCPReceiver::send() const
