2e8caf5451abbb0b4cbef6da62405f431449dbb1
CS144 user <emilio18@stanford.edu>
2024-02-02 06:47:55 +0000

    Trying to figure out unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,30 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + n;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
+  const Wrap32 wrapped_checkpoint = wrap(checkpoint, zero_point);
+  const int64_t signed_wrapped_checkpoint = wrapped_checkpoint.raw_value_;
+  int64_t diff = wrapped_checkpoint.raw_value_ - checkpoint;
+
+
+  const int64_t high_diff = wrapped_checkpoint.raw_value_ - checkpoint;
+  const int64_t low_diff = checkpoint - wrapped_checkpoint.raw_value_;
+
+
+
+
+
+  if (diff == 0) {
+    return checkpoint;
+  }
+  if (diff < 0) {
+    return checkpoint + 2**(32)
+  }
+
+ 
   return {};
 }
