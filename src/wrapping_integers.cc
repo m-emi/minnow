b5e8ed9b4aa74e6665408292cede3445c44da3b1
CS144 user <emilio18@stanford.edu>
2024-02-02 08:37:36 +0000

    Implemented correct unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -9,19 +9,17 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  const Wrap32 wrapped_checkpoint = wrap(checkpoint, zero_point);
-  uint64_t high_diff = wrapped_checkpoint.raw_value_ - checkpoint;
-  uint64_t low_diff = checkpoint - wrapped_checkpoint.raw_value_;
+  const uint32_t wrapped_checkpoint = Wrap32::wrap(checkpoint, zero_point).raw_value_;
+  uint32_t high_diff = raw_value_ - wrapped_checkpoint;
+  uint32_t low_diff = wrapped_checkpoint - raw_value_;
   
   // Underflow has occured. 
-  if (low_diff > checkpoint || ) {
+  if (low_diff <= checkpoint && low_diff < high_diff) {
+    return checkpoint - low_diff;
+  }
+  else {
     return checkpoint + high_diff;
   }
-  if (low_diff > high_diff) {
-      return checkpoint + high_diff;
-    }
-  // raw value is 
-  return checkpoint - low_diff;
 
 
 
