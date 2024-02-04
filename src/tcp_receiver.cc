5f991224e6b87139f05e1906da19d7735241e2a5
CS144 user <emilio18@stanford.edu>
2024-02-04 00:42:42 +0000

    Figuring out edge cases in receiver method

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -4,34 +4,51 @@ using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-
-  // To access reassembler, use reassembler_.
-  reassembler_.insert(first_index, message.payload, message.SYN);
-  // Unwrap gets you absolute sequence number
-  uint64_t abseqno = message.seqno.Unwrap(n, zero_point);
-
-
-  
   if (message.RST) {
     // use set_error() from bytestream
   }
-
-
+  // Unwrap gets you absolute sequence number
+  // Find stream index.
+  // Should be absolute seqno - 1
+  uint64_t abs_seqno = message.seqno.unwrap(message.seqno, writer().bytes_pushed());
+  uint64_t stream_index = abs_seqno - 1;
 
   if (message.SYN) {
     // this segment is beginning of the byte stream
-    // conversation is starting
-    // guaranteed to be sent first
     // don't start conversation until SYN flag is receieved 
-    // first_unassembled_index is checkpoint, AKA bytes_pushed
-    Wrap32 first_seg = message.seqno;
-    uint64_t ISN = first_seg.unwrap(first_seg, writer().bytes_pushed())
+    ISN = message.seqno;
+  }
+  // Conversation can be started
+  if (ISN.has_value()) {
+      uint64_t abs_seqno = message.seqno.unwrap(ISN.value(), writer().bytes_pushed());
+
+      if (message.SYN && !message.FIN) {
+        reassembler_.insert(0, message.payload, message.FIN);
+      }
+      else if (!message.SYN && !message.FIN) 
+      {
+        
+      }
+      
+
 
 
   }
+
+  if (!message.SYN && !message.FIN) {
+    reassembler_.insert(stream_index, message.payload, false);
+  }
+
   if (message.FIN) {
     //this segment is the end of the byte stream
     // close conversation once FIN flag is received
+    uint64_t abs_seqno = message.seqno.unwrap(message.seqno, writer().bytes_pushed());
+    uint64_t stream_index = abs_seqno - 1;
+    reassembler_.insert(stream_index, message.payload, message.FIN);
+  }
+
+  if (reassembler_.reader().is_closed()) {
+
   }
 }
 
