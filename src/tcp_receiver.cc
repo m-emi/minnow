bf198cf02382c193dd0768ded2ea3aae37aac1a6
CS144 user <emilio18@stanford.edu>
2024-02-06 06:09:46 +0000

    send and receive methods handle a single SYM flag correctly

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,4 +1,5 @@
 #include "tcp_receiver.hh"
+#include <iostream> 
 
 using namespace std;
 
@@ -11,58 +12,55 @@ void TCPReceiver::receive( TCPSenderMessage message )
   // Unwrap gets you absolute sequence number
   // Find stream index.
   // Should be absolute seqno - 1
-
+  cerr << message.SYN;
+  cout << message.SYN;
   if (message.SYN) 
   {
     // this segment is beginning of the byte stream
     // don't start conversation until SYN flag is receieved 
     ISN = message.seqno; 
+    cerr << ISN.has_value();
   }
 
   if (message.FIN)
   {
+    // last segment of the bytestream has been sent
+    // Bytestream may still be open however
     FIN_recieved = true;
   }
   
   // // Conversation can be started
   if (ISN.has_value()) 
   {
-      uint64_t abs_seqno = message.seqno.unwrap(ISN.value(), writer().bytes_pushed());
+      //uint64_t abs_seqno = message.seqno.unwrap(ISN.value(), writer().bytes_pushed());
+      uint64_t stream_index = message.seqno.unwrap(ISN.value(), writer().bytes_pushed()) - 1;
 
-      if (message.SYN && !message.FIN) 
+      //if (message.SYN && !message.FIN) 
+      if (message.SYN)
       {
-        reassembler_.insert(abs_seqno, message.payload, message.FIN);
+        reassembler_.insert(stream_index, message.payload, message.FIN);
       }
 
-      else if (!message.SYN && !message.FIN) 
-      {
-        reassembler_.insert(abs_seqno + 1, message.payload, message.FIN);
-      }
+      // else if (!message.SYN && !message.FIN) 
+      // {
+      //   reassembler_.insert(abs_seqno + 1, message.payload, message.FIN);
+      // }
 
-      else if (message.FIN) 
-      {
-        // stream should be closed if all bytes have been passed into the stream 
-        // Bytes still might be left even if a FIN flag is passed
-        reassembler_.insert(abs_seqno + 1, message.payload, message.FIN);
-      }
-      
+      // else if (message.FIN) 
+      // {
+      //   // stream should be closed if all bytes have been passed into the stream 
+      //   // Bytes still might be left even if a FIN flag is passed
+      //   reassembler_.insert(abs_seqno + 1, message.payload, message.FIN);
+      // } 
   }
-
-  // if (message.FIN) {
-  //   //this segment is the end of the byte stream
-  //   // close conversation once FIN flag is received
-  //   uint64_t abs_seqno = message.seqno.unwrap(message.seqno, writer().bytes_pushed());
-  //   uint64_t stream_index = abs_seqno - 1;
-  //   reassembler_.insert(stream_index, message.payload, message.FIN);
-  // }
-
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
   TCPReceiverMessage message;
-  if (ISN.has_value()) {
 
+  if (ISN.has_value()) 
+  {
     // Check if FIN flag has been received and byte stream is fully popped and closed
     if (FIN_recieved && reader().is_finished())
     {
@@ -76,17 +74,14 @@ TCPReceiverMessage TCPReceiver::send() const
     // Only SYN flag has been received
     else
     {
+      cerr << "only SYN flag acknowledged";
       message.ackno = Wrap32::wrap(writer().bytes_pushed() + 1, ISN.value() );
     }
-    
-    
-   
-
-    // uint32_t offset = 1;
-    // message.ackno = message.ackno.value() + offset;
   }
-  // account for max window size
+
+  //account for max window size
   message.window_size = writer().available_capacity();
+
   if (writer().has_error()) {
     message.RST = true;
   }
